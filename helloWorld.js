6069938066bb760e6fd507cefe0a0853482febdc
Mahime <mahimail0007@gmail.com>
2023-02-16 15:34:36 +0000

    Updated to have user input

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,10 @@
 console.log("hello world")
 
 alert("Git is Awesome");
+
+// modifying to allow input from user 
+let person = prompt("Please enter your name");
+
+if (person != null) {
+  alert("My name is " + person)
+}
